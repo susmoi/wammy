5ee736ee424ce50cc6bec7d040c32d9564e01d95
Elizabeth Brown <40839745+susmoi@users.noreply.github.com>
2025-01-13 16:52:48 -0500

    Update knowledge.js

--- server/knowledge.js
+++ server/knowledge.js
@@ -37,19 +37,6 @@ The world continues to power ahead on renewable energy. As countries around the 
 
 export const knowledgeLizBrown = `
 
-Wammy knows Elizabeth Brown also known as Liz. He knows she is a researcher currently practicing within a studio in a Design Research and Strategy agency. Conducting research in this setting hones her capabilities for quality, efficiency, and craft. Before her consultancy position and Liz's other roles in industry, she practiced in academia. There she learned to execute rigorous, statistically sound research while working in behavioral research labs. From universities to agencies, she enjoys the research process. Collecting observations, developing hypotheses, designing studies, conducting experiments, analyzing results, connecting the dots and discovering insights, and finally, a favorite of her's, making news and sharing the results.
-
-Wammy knows that Liz's website is and her email is liz.alma.brown@gmail.com. Wammy know's her phone number, but will not give that out.
-
-Wammy knows Liz's research methods and skills. In-depth interviews, Focus groups, Contextual inquiries, Card sorting, Tree tests, Moderated and un-moderated usability studies, Diary studies, Workshopping, Field studies, Remote studies, Lab studies, Surveying, MaxDiff analysis.
-
-Wammy knows that Liz's research is focused within the experiences people have with technology, environments, and each other.
-
-Wammy knows that Liz is not a developer, but she could code a program in a pinch. She watches high class reality tv and binges trashy internet articles. She is currently taking a break from tree, though she is not 100% sober. She still likes a drink or two on the weekend.
-
 Wammy knows that Liz lives in Richmond, Va.
 
-Wammy knows that Liz's favorite number is 26. It knows that in general Liz likes round, even numbers.
-
-Wammy knows that Liz usually dresses in all black.
 `
